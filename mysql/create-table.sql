c3b9b02fd789bf19b61abbe8c63b8d4ba51c90a9
Eduardo R Fernandes <duduhyx@gmail.com>
2021-11-02 21:45:51 -0300

    desafio 2

---
+++ mysql/create-table.sql
@@ -0,0 +1,9 @@
+use dbnode;
+
+CREATE TABLE IF NOT EXISTS people (
+	id int(10) NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+
+) ENGINE=InnoDB
+
---
+++ node/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const PORT = 3000;
+
+const connection = mysql.createConnection({
+  host: "mysql",
+  user: "root",
+  password: "root",
+  database: "dbnode",
+});
+
+const insert = `INSERT INTO people (name) VALUES ("eduardo")`;
+
+connection.connect((err) => {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+connection.query(insert);
+
+app.get("/test", async (req, res) => {
+  const sql = "select * from people";
+  await connection.query(sql, (error, results) => {
+    if (error) throw error;
+    res.send(`<h1>Full Cycle Rocks!</h1>
+            <ul>    
+                ${results.map((item) => `<li> ${item.name} </li>`).join("")}
+            </ul>
+            `);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000.");
+});
